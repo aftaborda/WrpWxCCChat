9250ddc4b6764055573c7f621500c87cded098a0
António Taborda <antonio.taborda@warpcom.com>
2026-09-06 23:24:35 +0100

    env

--- src/routes/wxcc.js
+++ src/routes/wxcc.js
@@ -1,9 +1,12 @@
 import express from "express";
 const wxccRouter = express.Router();
 
+const WEBEXAPIS = process.env.WEBEX_APIS_V1;
+const WEBEX_API_SERVER = process.env.WEBEX_API_SERVER;
+
 /* GET users listing. */
 wxccRouter.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+  res.send(`WEBEXAPIS: ${WEBEXAPIS}, WEBEX_API_SERVER: ${WEBEX_API_SERVER}`);
 });
 
 export { wxccRouter };
